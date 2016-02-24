66d94c45ae23f44cb9a66cc224497a0c5d5b0713
Andrey Mikhaylin <cenobith@gmail.com>
2016-02-24 21:06:42 +0300

    Index on user_id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209185009) do
+ActiveRecord::Schema.define(version: 20160224180448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20160209185009) do
     t.datetime "updated_at",                 null: false
     t.boolean  "published",  default: false
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "profiles", force: :cascade do |t|
     t.string   "nickname"
